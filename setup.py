63d932bd8551567b1cc585f63859343c5e2bd558
manuelarcer <81344717+manuelarcer@users.noreply.github.com>
2023-03-10 17:05:12 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#from setuptools import setup, find_packages
+
+setup(
+    name = 'asetools',
+    version = '0.1',
+    url = '',
+    description = '',
+    #packages = find_packages(),
+    #install_requires = [
+    #    # Github Private Repository
+    #    'ExampleRepo @ git+ssh://git@github.com/example_org/ExampleRepo.git'
+    #]
+)
