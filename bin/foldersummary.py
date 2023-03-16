2843f51d5dd373eb6720e44eaa16885d27e0ef5c
manuelarcer <manuel.arceram@gmail.com>
2023-03-16 15:13:44 +0800

    Create foldersummay bin file

---
+++ bin/foldersummary.py
@@ -0,0 +1,16 @@
+from ase.io import read
+import pandas as pd
+import numpy as np
+import glob, os
+from asetools.analysis import check_energy_and_maxforce, check_outcar_convergence
+
+
+folders = glob.glob('*/')
+
+for f in folders:
+    if os.path.exists(f+'OUTCAR'):
+        converged = check_outcar_convergence(f+'OUTCAR')
+        energy, maxforce = check_energy_and_maxforce()
+
+    else:
+        print('No OUTCAR in ', f)
