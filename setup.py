37185afcf9817f3118dff636f261f3e09745d9a8
manuelarcer <manuel.arceram@gmail.com>
2023-03-16 16:29:40 +0800

    Created dummy setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+# as suggested in the following
+# https://towardsdatascience.com/setuptools-python-571e7d5500f2#:~:text=be%20more%20appropriate.-,The%20setup.,as%20the%20command%20line%20interface.
+
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
