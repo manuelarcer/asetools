71b28f690588d53b043e0b5d020684c48df2e589
manuelarcer <manuel.arceram@gmail.com>
2023-03-16 13:57:56 +0800

    Created convergence function

--- analysis.py
+++ analysis.py
@@ -3,9 +3,38 @@
 
 from ase.io import read
 import pandas as pd
-from vasprun import vasprun
-
-
-def check_convergence(xml='vasprun.xml'):
-    vasp = vasprun('vasprun.xml')
     
+def check_outcar_convergence(outcar, verbose=True):
+    out = open(outcar, 'r')
+    lines = out.readlines()
+    ibrion = None
+    nsw = None
+    opt = False
+    convergence = False
+    for line in lines:
+        if 'IBRION' in line:
+            ibrion = int( line.split()[2] )
+            if ibrion == 1 or ibrion == 2:
+                opt = True
+            elif verbose:
+                print(f'IBRION --> {ibrion}, not an OPTIMIZATION job')
+                opt = False
+            else:    
+                opt = False
+        elif 'NSW' in line:
+            nsw = int( line.split()[2] )
+            if nsw > 0:
+                opt = True
+        elif 'reached required accuracy - stopping' in line and opt:
+            convergence = True
+    if verbose:
+        print(f'IBRION --> {ibrion}, NSW --> {nsw}')    
+    
+    if ibrion == 1 or ibrion == 2:
+        if nsw > 0 and convergence:
+            print('Optimization Job --> CONVERGED')
+        elif nsw > 0 and not convergence and verbose:
+            print('Optimization Job --> *NOT* converged')
+            
+    return convergence
+
