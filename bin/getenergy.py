2061141c1b3aec7ef04054abc0a2e3f4d4a9e7bc
Juan <manuel.arceram@gmail.com>
2023-03-20 17:27:00 +0800

    Created script to get energies of single folder

---
+++ bin/getenergy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from asetools.analysis import check_energy_and_maxforce, check_outcar_convergence
+import sys
+
+if sys.argv[1] is None:
+    outcar = 'OUTCAR'
+else:
+    outcar = sys.argv[1]
+
+converged = check_outcar_convergence(outcar, verbose=True)
+energy, maxforce = check_energy_and_maxforce(outcar, magmom=False, verbose=False)
+
+print('{} {} {}'.format('Converged', 'MaxForce', 'Energy'))
+print(f'{converged:10s} {maxforce:10.3f} {energy:10.4f}')
+
