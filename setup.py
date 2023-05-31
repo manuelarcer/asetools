a62022beeaef07c8151df69dbf7e39d1b86bd331
Juan <manuel.arceram@gmail.com>
2023-05-31 10:25:39 +0800

    Move packages and prepare for public use

--- setup.py
+++ setup.py
@@ -3,4 +3,22 @@
 
 from setuptools import setup
 if __name__ == '__main__':
-    setup()
+    setup(
+        name='asetools',
+        version='0.1.0',
+        description='Complement to ASE to perform analysis and manipulate atoms',
+        author='Juan M Arce-Ramos',
+        author_email='manuel.arceram@gmail.com',
+        url='https://github.com/manuelarcer/asetools.git',
+        packages=find_packages(),
+        install_requires=[
+        # List of packages your project depends on
+        entry_points={
+            'console_scripts': [
+                'executable_script1=asetools.bin.executable_script1:main',
+                'executable_script2=asetools.bin.executable_script2:main',
+                # etc.
+            ],
+        }
+        ...
+    )
