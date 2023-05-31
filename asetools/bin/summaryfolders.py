cbacf0c6f330b645a7ebf786f77fdd716a4ea064
Juan <manuel.arceram@gmail.com>
2023-05-31 10:27:57 +0800

    main function in bin files

--- asetools/bin/summaryfolders.py
+++ asetools/bin/summaryfolders.py
@@ -6,21 +6,24 @@ import numpy as np
 import glob, os
 from asetools.analysis import check_energy_and_maxforce, check_outcar_convergence
 
+def main():
+    folders = glob.glob('*/')
 
-folders = glob.glob('*/')
+    dic = {'Config': [], 'Converged':[], 'MaxForce': [], 'Energy':[]}
+    for f in sorted(folders):
+        if os.path.exists(f+'OUTCAR'):
+            converged = check_outcar_convergence(f+'OUTCAR', verbose=False)
+            energy, maxforce = check_energy_and_maxforce(f+'OUTCAR', magmom=False, verbose=False)
+            dic['Config'].append(f)
+            dic['Converged'].append(converged)
+            dic['MaxForce'].append(maxforce)
+            dic['Energy'].append(energy)
 
-dic = {'Config': [], 'Converged':[], 'MaxForce': [], 'Energy':[]}
-for f in sorted(folders):
-    if os.path.exists(f+'OUTCAR'):
-        converged = check_outcar_convergence(f+'OUTCAR', verbose=False)
-        energy, maxforce = check_energy_and_maxforce(f+'OUTCAR', magmom=False, verbose=False)
-        dic['Config'].append(f)
-        dic['Converged'].append(converged)
-        dic['MaxForce'].append(maxforce)
-        dic['Energy'].append(energy)
-
-    else:
-        print('No OUTCAR in ', f)
+        else:
+            print('No OUTCAR in ', f)
     
-df = pd.DataFrame.from_dict(dic) 
-print(df)
+    df = pd.DataFrame.from_dict(dic) 
+    print(df)
+
+if __name__ == "__main__":
+    main()
