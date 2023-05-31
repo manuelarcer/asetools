06a0bc9623bae5560b81a5ff85e9028c6fa65f1a
Juan <manuel.arceram@gmail.com>
2023-05-31 10:50:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-    setup(
-        name='asetools',
-        version='0.1.0',
-        description='Complement to ASE to perform analysis and manipulate atoms',
-        author='Juan M Arce-Ramos',
-        author_email='manuel.arceram@gmail.com',
-        url='https://github.com/manuelarcer/asetools.git',
-        packages=find_packages(),
-        install_requires=[
-        # List of packages your project depends on
-        entry_points={
-            'console_scripts': [
-                'executable_script1=asetools.bin.getenergy:main',
-                'executable_script2=asetools.bin.summaryfolders:main',
-                # etc.
-            ],
-        }
-        ...
-    )
+    setup()
