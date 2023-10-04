3c5c0f8e187fc902e1346d40c8a86153f0ef4bff
Juan <manuel.arceram@gmail.com>
2023-10-04 10:59:21 +0800

    Modified version by ChatGPT

--- asetools/bin/genpotentialdepcalc.py
+++ asetools/bin/genpotentialdepcalc.py
@@ -1,61 +1,21 @@
 #!/usr/bin/env python
 
-from ase.io import read
-import os, shutil, sys
+import os
+import shutil
+import argparse
 import numpy as np
+from ase.io import read
+
+VALANCES = {'Cu': 11, 'Zn': 12, 'C': 4, 'O': 6, 'H': 1}
+EPSILON = 0.001
 
 def get_sum_electrons(poscar):
-    valences = {'Cu':11, 'Zn':12, 'C':4, 'O':6, 'H':1}
     atoms = read(poscar)
     symbols = atoms.get_chemical_symbols()
-    elecpersymb = [valences[symb] for symb in symbols ]
+    elecpersymb = [VALANCES[symb] for symb in symbols]
     return sum(elecpersymb)
 
-print()
-print('Preparation of folders for Potential-dependent calculations')
-print('USAGE:   genporentialdepcalc.py   POSCAR   pythonfile.py   lower_n_e(OPT)   higher_n_e(OPT)  step(OPT)')
-print('pythonfile.py, is the script used for the calculations, all ase-related lines for the calculations are here')
-print('lower_n_e, represent the lower limit for the number of electrons added to the neutral system')
-print()
-
-## Parameters ########
-poscar = sys.argv[1]
-pythonfile = sys.argv[2]
-
-if len(sys.argv) == 3:
-    lower = -1
-    higher = 1
-    step = 0.2
-elif len(sys.argv) == 4:
-    lower = float( sys.argv[3] )
-    higher = -lower
-    step = 0.2
-elif len(sys.argv) == 5:
-    lower = float( sys.argv[3] )
-    higher = float( sys.argv[4] )
-    step = 0.2
-elif len(sys.argv) == 6:
-    lower = float( sys.argv[3] )
-    higher = float( sys.argv[4] )
-    step = float( sys.argv[5] )
-else:
-    print('Please verify the input parameters')
-######################
-
-numelec = get_sum_electrons(poscar)
-nelec_min = numelec + lower
-nelec_max = numelec + higher
-
-print(f'Num_elec (Neutral) = {numelec} ; lower = {nelec_min} ; higher = {nelec_max}')
-
-for i, ne in enumerate( np.arange(nelec_min, nelec_max + 0.001, step) ):
-    
-    name = f'{i:02d}_nelec_{ne:.2f}'
-    os.mkdir(name)
-    shutil.copyfile(pythonfile, name+'/'+pythonfile)
-    shutil.copyfile(poscar, name+'/'+poscar)
-
-    os.chdir(name)
+def modify_python_file(pythonfile, ne):
     with open(pythonfile, 'r') as file:
         content = file.readlines()
         for k, line in enumerate(content):
@@ -67,4 +27,31 @@ for i, ne in enumerate( np.arange(nelec_min, nelec_max + 0.001, step) ):
     with open(pythonfile, 'w') as file:
         file.writelines(content)
 
-    os.chdir('..')
+def main():
+    parser = argparse.ArgumentParser(description='Preparation of folders for Potential-dependent calculations')
+    parser.add_argument('poscar', help='Path to POSCAR file')
+    parser.add_argument('pythonfile', help='Script used for the calculations')
+    parser.add_argument('--lower', type=float, default=-1, help='Lower limit for the number of electrons')
+    parser.add_argument('--higher', type=float, default=1, help='Higher limit for the number of electrons')
+    parser.add_argument('--step', type=float, default=0.2, help='Step value for electron count')
+
+    args = parser.parse_args()
+
+    numelec = get_sum_electrons(args.poscar)
+    nelec_min = numelec + args.lower
+    nelec_max = numelec + args.higher
+
+    print(f'Num_elec (Neutral) = {numelec} ; lower = {nelec_min} ; higher = {nelec_max}')
+
+    for i, ne in enumerate(np.arange(nelec_min, nelec_max + EPSILON, args.step)):
+        name = f'{i:02d}_nelec_{ne:.2f}'
+        os.mkdir(name)
+        shutil.copyfile(args.pythonfile, os.path.join(name, args.pythonfile))
+        shutil.copyfile(args.poscar, os.path.join(name, args.poscar))
+
+        os.chdir(name)
+        modify_python_file(args.pythonfile, ne)
+        os.chdir('..')
+
+if __name__ == "__main__":
+    main()
