22e155942aaea7a4e98230f77bbdbaa675777a51
Juan <manuel.arceram@gmail.com>
2024-03-27 14:14:04 +0800

    changes

--- asetools/bin/sortposcarbyelement.py
+++ asetools/bin/sortposcarbyelement.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 from ase.io import read, write
 from ase import Atoms
 from ase.constraints import FixAtoms
@@ -10,44 +11,68 @@ def get_fixed_indices(atoms):
             return constraint.index
     return []
 
-def sort_atoms(filename, element_order):
+def sort_atoms(filename, element_order, sort_axis='z', ascending=True):
     atoms = read(filename)
     fixed_indices = get_fixed_indices(atoms)
 
-    sorted_atoms = Atoms(cell=atoms.cell, pbc=atoms.pbc)
-    index_map = {}  # Maps old indices to new indices
-    new_fixed_indices = []
-
-    # Sort the atoms and track index changes
+    # Sorting by element type first
+    sorted_atoms_by_type = []
     for element in element_order:
-        for i, atom in enumerate(atoms):
-            if atom.symbol == element:
-                sorted_atoms.append(atom)
-                index_map[i] = len(sorted_atoms) - 1
+        sorted_atoms_by_type.extend([atom for atom in atoms if atom.symbol == element])
 
-    # Update the fixed indices based on the sorted atoms
+    # Sorting within each type by specified axis
+    axis_indices = {'x': 0, 'y': 1, 'z': 2}
+    axis_index = axis_indices.get(sort_axis, 2)  # Default to z-axis if unspecified
+    sorted_atoms_by_type_and_axis = []
+    for element in element_order:
+        atoms_of_element = [atom for atom in sorted_atoms_by_type if atom.symbol == element]
+        sorted_atoms_of_element = sorted(atoms_of_element, key=lambda atom: atom.position[axis_index], reverse=not ascending)
+        sorted_atoms_by_type_and_axis.extend(sorted_atoms_of_element)
+
+    # Create new Atoms object with sorted atoms
+    new_atoms = Atoms(cell=atoms.cell, pbc=atoms.pbc)
+    for atom in sorted_atoms_by_type_and_axis:
+        new_atoms.append(atom)
+
+    # Update the fixed indices based on the new sorted order
+    new_fixed_indices = []
     for old_index in fixed_indices:
-        new_fixed_indices.append(index_map[old_index])
+        for new_index, atom in enumerate(new_atoms):
+            if atom.index == old_index:
+                new_fixed_indices.append(new_index)
+                break
 
     if new_fixed_indices:
-        sorted_atoms.set_constraint(FixAtoms(indices=new_fixed_indices))
+        new_atoms.set_constraint(FixAtoms(indices=new_fixed_indices))
 
-    return sorted_atoms
+    return new_atoms
 
 def main():
-    import sys
-    if len(sys.argv) < 3:
-        print("Usage: script.py <filename> <element1> <element2> ...")
+    if len(sys.argv) < 4:
+        print("Usage: script.py <filename> <element1> <element2> ... [-axis x|y|z] [-desc]")
         sys.exit(1)
 
     filename = sys.argv[1]
     element_order = sys.argv[2:]
 
-    sorted_atoms = sort_atoms(filename, element_order)
+    # Detect if sorting by axis is specified
+    sort_axis = 'z'  # Default axis
+    ascending = True  # Default order
+    if '-axis' in element_order:
+        axis_index = element_order.index('-axis')
+        sort_axis = element_order[axis_index + 1].lower()
+        element_order = element_order[:axis_index]  # Remove axis options from element_order list
+        if '-desc' in element_order:
+            ascending = False
+            element_order.remove('-desc')
+
+    sorted_atoms = sort_atoms(filename, element_order, sort_axis, ascending)
 
     # Save the sorted structure with preserved constraints
     write('sorted_POSCAR', sorted_atoms, format='vasp')
 
 if __name__ == "__main__":
     main()
+
+
 
