2cd828021c34ef6dc8936dce40ffb2662009be54
Juan <manuel.arceram@gmail.com>
2024-08-05 22:39:40 +0800

    fast mode

--- asetools/bin/summaryfolders.py
+++ asetools/bin/summaryfolders.py
@@ -13,6 +13,8 @@ def main():
     parser = argparse.ArgumentParser(description='Process folders.')
     parser.add_argument('-m', '--magmom', action='store_true',
                         help='extract and present magnetic moments')
+    parser.add_argument('-f', '--fast', action='store_true',
+                        help='fast mode (reading out file)')
     args = parser.parse_args()
     ##
 
@@ -21,8 +23,47 @@ def main():
         dic = {'Config': [], 'Converged':[], 'MaxForce': [], 'Energy':[], 'MagMom':[]}
     else:
         dic = {'Config': [], 'Converged':[], 'MaxForce': [], 'Energy':[]}
+
+    # Define alternative filenames to look for when fast mode is enabled
+    alternative_filenames = ['vasp.out', 'out.txt']
+
     for f in sorted(folders):
-        if os.path.exists(f + 'OUTCAR'):
+        
+        ########## Block for fast mode ########
+        if args.fast:
+            for alt in alternative_filenames:
+                if os.path.exists(f + alt):
+                    vaspout = open(f + alt, 'r')
+                    break
+            if not vaspout:
+                print('No OUT file in', f)
+                continue
+            
+            incar = open(f + 'INCAR', 'r')
+            incarlines = incar.readlines()
+            ibrion = None
+            for line in incarlines:
+                if 'IBRION' in line:
+                    ibrion = int(line.split()[2])
+                    break
+            lines = vaspout.readlines()
+            if ibrion == 1 or ibrion == 2 or ibrion == 3:
+                for line in lines:
+                    if 'reached required accuracy' in line:
+                        converged = True
+                        break
+                    else:
+                        converged = False
+            else:
+                for line in lines:
+                    if 'E0=' in line:
+                        converged = True
+                        break
+                    else:
+                        converged = False
+        #########################################
+
+        else os.path.exists(f + 'OUTCAR'):
             try:
                 converged = check_outcar_convergence(f + 'OUTCAR', verbose=False)
                 if args.magmom:
