096fe51842e08c0e860981c6c80e1cf964d8b5de
Juan <manuel.arceram@gmail.com>
2024-09-24 16:34:28 +0800

    Uptade file

--- asetools/bin/view_outcars.py
+++ asetools/bin/view_outcars.py
@@ -9,16 +9,39 @@ from asetools.analysis import check_outcar_convergence
 def main():
     parser = argparse.ArgumentParser(description='Visualize the last configuration in OUTCAR files.')
     parser.add_argument('--converged', action='store_true', help='Only consider converged calculations')
+    parser.add_argument('--folders', '-f', nargs='+', help='List specific folders to process. If not provided, all folders in the current directory will be processed.')
     args = parser.parse_args()
     
+    if args.folders:
+        # User has specified folders to process
+        folders_to_process = args.folders
+        valid_folders = []
+        for folder in folders_to_process:
+            if os.path.isdir(folder):
+                valid_folders.append(folder)
+            else:
+                print(f'WARNING: {folder} is not a valid directory, skipping...')
+        if not valid_folders:
+            print('No valid folders provided. Exiting')
+            return
+    else:
+        # Process all folders in the current directory
+        valid_folders = [f for f in sorted( os.listdir('.') ) if os.path.isdir(f)]
+
     listatoms = []
-    for f in sorted( os.listdir('.') ):
+    for f in valid_folders:
         outcar_path = os.path.join(f, 'OUTCAR')
         if os.path.isfile(outcar_path):
             converged, _ = check_outcar_convergence(outcar_path, verbose=False)
             if not args.converged or converged:
-                atoms = read(outcar_path, format='vasp-out', index=-1)
-                listatoms.append(atoms)
+                try:
+                    atoms = read(outcar_path, format='vasp-out', index=-1)
+                    listatoms.append(atoms)
+                except Exception as e:
+                    print(f'Error reading {outcar_path}: {e}')
+        else:
+            print(f'OUTCAR not found in {f}, skipping...')
+            
     if listatoms:
         view(listatoms)
     else:
